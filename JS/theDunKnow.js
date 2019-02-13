6172b58a430e8f5a141aaa633fa3fb03f2f402e5
Status100 <bluemagicuk@yahoo.co.uk>
2019-02-13 10:47:19 +0000

    add tab click and class functionality using jQuery

--- JS/theDunKnow.js
+++ JS/theDunKnow.js
@@ -14,3 +14,24 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+$(document).ready(function() { 
+
+	(function ($) { 
+		$('.showcase ul.showcaseTabs').addClass('active').find('> li:eq(0)').addClass('current');
+		
+		$('.showcase ul.showcaseTabs li a').click(function (g) { 
+			var tab = $(this).closest('.showcase'), 
+				index = $(this).closest('li').index();
+			
+			tab.find('ul.showcase > li').removeClass('current');
+			$(this).closest('li').addClass('current');
+			
+			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+			
+			g.preventDefault();
+		} );
+	})(jQuery);
+
+});
