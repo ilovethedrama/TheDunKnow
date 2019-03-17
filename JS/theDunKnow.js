e4574f3b56b93f27d9daef7ed38aec86644884c2
Status100 <bluemagicuk@yahoo.co.uk>
2019-03-17 23:04:56 +0000

    mop up JS file

--- JS/theDunKnow.js
+++ JS/theDunKnow.js
@@ -10,7 +10,7 @@ var headingTitle = document.getElementById("scrollTitle");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-var sticky1 = headingTitle.offsetTop;
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navBarStick() {
